188d7d927d2bf269c4b45540068c86a185cbceb6
lu hou <luhou.me@gmail.com>
2023-05-03 15:02:33 -0700

    704

---
+++ array/binary_search.ts
@@ -0,0 +1,21 @@
+// input nums=,target = 3
+// output 2
+// find the index of the target in an array
+
+function binarySearch(arr: number[], target: number) {
+  let left = 0,
+    right = arr.length;
+  while (left < right) {
+    let mid = Math.floor(left + (right - left) / 2);
+    if (arr[mid] > target) {
+      right = mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}
+const a = binarySearch([-1, 0, 3, 5, 6, 12], 7);
+console.log("result = ", a);
