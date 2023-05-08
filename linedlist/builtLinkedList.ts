4e868504a01baf95fe4d2e08758be1c36645ce98
lu hou <luhou.me@gmail.com>
2023-05-07 19:23:09 -0700

    707

--- linedlist/builtLinkedList.ts
+++ linedlist/builtLinkedList.ts
@@ -1,104 +1,114 @@
-class ListNode {
+// Design your implementation of the linked list. You can choose to use a singly or doubly linked list.
+// A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node.
+// If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
+//
+// Implement the MyLinkedList class:
+//
+// MyLinkedList() Initializes the MyLinkedList object.
+// int get(int index) Get the value of the indexth node in the linked list. If the index is invalid, return -1.
+// void addAtHead(int val) Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+// void addAtTail(int val) Append a node of value val as the last element of the linked list.
+// void addAtIndex(int index, int val) Add a node of value val before the indexth node in the linked list. If index equals the length of the linked list, the node will be appended to the end of the linked list. If index is greater than the length, the node will not be inserted.
+// void deleteAtIndex(int index) Delete the indexth node in the linked list, if the index is valid.
+
+class SinglyListNode {
   val: number;
-  next: ListNode | null;
-  constructor(val: number, next: ListNode | null = null) {
+  next: SinglyListNode | null;
+  constructor(val: number = 0, next: SinglyListNode | null = null) {
     this.val = val;
     this.next = next;
   }
 }
 class Mylinkedlist {
-  private size: number;
-  private head: ListNode | null;
-  private tail: ListNode | null;
+  dummy: SinglyListNode;
+  size: number;
   constructor() {
+    this.dummy = new SinglyListNode();
     this.size = 0;
-    this.head = null;
-    this.tail = this.head;
   }
-  append(val: number) {
-    let newNode = new ListNode(val);
-    if (!this.tail) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
+  traversToIndex(index: number): SinglyListNode {
+    let count = 0;
+    let cur = this.dummy;
+    while (index >= 0) {
+      cur = cur.next!;
+      index--;
     }
-    this.size++;
-    return this;
-  }
-  prepend(value: number) {
-    let newNode = new ListNode(value);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-    this.size++;
-    return this;
+    return cur;
   }
   printlist() {
     const arr: number[] = [];
-    let cur = this.head;
+    let cur = this.dummy.next;
     while (cur !== null) {
       arr.push(cur.val);
       cur = cur.next;
     }
     return arr;
   }
-  Insert(index: number, val: number) {
-    if (index >= this.size) {
-      return this.append(val);
+  get(index: number): number {
+    if (index < 0 || index >= this.size) {
+      return -1;
     }
-    if (index === 0) {
-      return this.prepend(val);
+    let cur: SinglyListNode = this.dummy;
+    while (index >= 0 && cur.next !== null) {
+      cur = cur.next;
+      index--;
     }
-    const newNode = new ListNode(val);
-    const leader = this.traversToIndex(index - 1);
-    if (leader) {
-      const holdingPoint = leader.next;
-      leader.next = newNode;
-      newNode.next = holdingPoint;
-      this.size++;
-      return this.printlist();
-    }
+    return cur.val;
   }
-  traversToIndex(index: number) {
-    let counter = 0;
-    let cur = this.head;
-    while (counter !== index && cur !== null) {
-      cur = cur?.next;
-      counter++;
-    }
-    return cur;
+  addAtHead(value: number): void {
+    const cur = this.dummy.next;
+    const newNode = new SinglyListNode(value, cur);
+    this.dummy.next = newNode;
+    this.size++;
   }
-  remove(index: number) {
-    if (index > 0 || index < this.size) {
+  addAtTail(val: number): void {
+    const newNode = new SinglyListNode(val);
+    let cur = this.dummy;
+    while (cur && cur.next) {
+      cur = cur.next;
+    }
+    cur.next = newNode;
+    this.size++;
+  }
+  addAtIndex(index: number, val: number): void {
+    if (index <= 0) {
+      this.addAtHead(val);
+    } else if (index >= this.size) {
+      this.addAtTail(val);
+    } else {
+      const newNode = new SinglyListNode(val);
       const leader = this.traversToIndex(index - 1);
-      if (leader) {
+      // point newNode.next to the instert position(leader.next)
+      newNode.next = leader.next;
+      // point leader.next to newNode
+      leader.next = newNode;
+      this.size++;
+    }
+  }
+  deleteAtindex(index: number): void {
+    if (index > 0 && index < this.size) {
+      const leader = this.traversToIndex(index - 1);
+      if (leader.next) {
         const unwanted = leader.next;
-        if (unwanted) {
-          leader.next = unwanted.next;
-        }
-
-        this.size--;
-      }
-
-      if (index === 0 && this.head) {
-        this.head = this.head.next;
+        leader.next = unwanted.next;
         this.size--;
       }
     }
-    return this.printlist();
+    if (index < 0 || index >= this.size) return;
+    if (index === 0 && this.dummy.next) {
+      this.dummy = this.dummy.next;
+      this.size--;
+    }
   }
 }
-const mylinedlist = new Mylinkedlist();
-mylinedlist.append(10);
-mylinedlist.append(15);
-mylinedlist.append(12);
-// mylinedlist.prepend(1);
-mylinedlist.Insert(0, 3);
-mylinedlist.remove(0);
-console.log(mylinedlist.printlist());
+const mylinkedlist = new Mylinkedlist();
+mylinkedlist.addAtHead(5);
+mylinkedlist.addAtTail(6);
+mylinkedlist.addAtHead(4);
+console.log(mylinkedlist.printlist());
+
+mylinkedlist.get(0);
+mylinkedlist.addAtIndex(2, 7);
+mylinkedlist.deleteAtindex(0);
+
+console.log(mylinkedlist.printlist());
--- linedlist/removeElement.ts
+++
@@ -1,8 +0,0 @@
-function removeElements(head: ListNode | null, val: number) {
-  let dmy = new ListNode(0, head);
-  let cur = dmy;
-  while (cur.next !== null) {
-    cur.next.val === val ? (cur.next = cur.next.next) : (cur = cur.next);
-  }
-  return dmy.next;
-}
