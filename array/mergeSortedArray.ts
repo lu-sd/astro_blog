ebe3a378ebec71558ce28a6e8246b08f3757ed24
lu hou <luhou.me@gmail.com>
2023-05-07 22:31:05 -0700

    88

---
+++ array/mergeSortedArray.ts
@@ -0,0 +1,36 @@
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+//
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+// 由于 nums1 的有效部分和 nums2 并不一定是一样长的。我们还需要考虑其中一个提前到头的这种情况：
+// 如果提前遍历完的是 nums1 的有效部分，剩下的是 nums2。那么这时意味着 nums1 的头部空出来了，直接把 nums2 整个补到 nums1 前面去即可。
+// 如果提前遍历完的是 nums2，剩下的是 nums1。由于容器本身就是 nums1，所以此时不必做任何额外的操作。
+function mergeSortedArray(
+  nums1: number[],
+  m: number,
+  nums2: number[],
+  n: number
+): void {
+  let i = m - 1,
+    j = n - 1;
+  let k = m + n - 1;
+  //当两个数组都没遍历完时，指针同步移动
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] >= nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+      k--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+      k--;
+    }
+  }
+  //nums2 留下的情况，特殊处理一下
+  while (j >= 0) {
+    nums1[k] = nums2[j];
+    k--;
+    j--;
+  }
+}
