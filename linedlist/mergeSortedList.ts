b1c739241e32105f04041ca1b1538528853e9e69
lu hou <luhou.me@gmail.com>
2023-05-09 06:37:14 -0700

    21

---
+++ linedlist/mergeSortedList.ts
@@ -0,0 +1,34 @@
+//You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val: number = 0, next: ListNode | null = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+function mergeTwoList(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  // difine head node
+  let head = new ListNode();
+  let cur = head;
+  while (l1 && l2) {
+    if (l1.val <= l2.val) {
+      cur.next = l1;
+      l1 = l1.next;
+    } else {
+      cur.next = l2;
+      l2 = l2.next;
+    }
+    cur = cur.next;
+  }
+  // deal with different length of two list
+  cur.next = l1 ? l1 : l2;
+  return head.next;
+}
