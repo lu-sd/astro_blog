bf4c653589836527b1de6b19c1f6c9d5d38bed92
lu hou <luhou.me@gmail.com>
2023-05-10 11:09:10 -0700

    use three pointers

--- linedlist/reverseLinkedlist.ts
+++ linedlist/reverseLinkedlist.ts
@@ -18,3 +18,23 @@ function reverseLinkedList(head: ListNode | null): ListNode | null {
   }
   return newlist;
 }
+
+function reverseList(head: ListNode | null): ListNode | null {
+  // Initialize the predecessor node to null
+  let pre: ListNode | null = null;
+  // Initialize the current node to the head node
+  let cur: ListNode | null = head;
+  // Traverse the list as long as the current node is not null
+  while (cur !== null) {
+    // Record the next node
+    let next: ListNode | null = cur.next;
+    // Reverse the pointer
+    cur.next = pre;
+    // Move the predecessor node one step forward
+    pre = cur;
+    // Move the current node one step forward
+    cur = next;
+  }
+  // After the reversal is complete, the predecessor node becomes the new head node
+  return pre;
+}
