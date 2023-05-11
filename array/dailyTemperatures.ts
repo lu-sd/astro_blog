f2b65cd9cf427f74c8b80abcf53222918ceb516e
lu hou <luhou.me@gmail.com>
2023-05-11 14:19:52 -0700

    add  comments

--- array/dailyTemperatures.ts
+++ array/dailyTemperatures.ts
@@ -3,16 +3,23 @@
 // Output: [1,1,4,2,1,1,0,0]
 function dailyTemperatures(temperatures: number[]): number[] {
   const t = temperatures;
+  //Create an array to hold the result with the same length as the input array and fill it with 0
   const answer = new Array(t.length).fill(0);
   const stack: number[] = [];
   if (t.length <= 1) return answer;
+  // Traverse the input array from left to right
   for (let i = 0; i < t.length; i++) {
-    while (t[stack[stack.length - 1]] < t[i]) {
+    // While the stack is not empty and the temperature at the top of the stack is less than the temperature at the current index
+    while (stack.length > 0 && t[stack[stack.length - 1]] < t[i]) {
+      // Pop the top index from the stack
       const top = stack.pop() as number;
+      // Calculate the number of days until the next warmer day and store it in the answer array at the top index
       answer[top] = i - top;
     }
+    // Push the current index onto the stack
     stack.push(i);
   }
+  // Return the answer array
   return answer;
 }
 let b = dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]);
